d444503fd30118d7c9ffbcb5ebd98a58d437a9e9
Kaden Almizyed <kalmizyed@wisc.edu>
2022-02-22 15:22:57 -0600

    Added scenario comments to all tests

--- FrontendDeveloperTests.java
+++ FrontendDeveloperTests.java
@@ -9,6 +9,30 @@ public class FrontendDeveloperTests {
      */
     public static boolean testDisplayCommandMenu() {
         try {
+            // SCENARIO 1: Test valid input
+            ///////////////////////////////
+
+            // 1A: lowercase character
+
+            // 1B: uppercase character
+
+            // 1C: number
+
+            // 1D: quit
+
+
+            // SCENARIO 2: Test invalid input
+            /////////////////////////////////
+
+            // 2A: invalid lowercase character
+
+            // 2B: invalid uppercase character
+
+            // 2C: invalid number
+
+            // 2D: string longer than 1 character
+
+            // 2E: invalid character
 
         } catch (Exception e) {
             System.out.println("FAILED: testDisplayCommandMenu(); unexpected exception");
@@ -24,6 +48,12 @@ public class FrontendDeveloperTests {
      */
     public static boolean testDisplayShows() {
         try {
+            // SCENARIO 1: Test displaying empty list
+            /////////////////////////////////////////
+
+
+            // SCENARIO 2: Test displaying filled list
+            //////////////////////////////////////////
 
         } catch (Exception e) {
             System.out.println("FAILED: testDisplayShows(); unexpected exception");
@@ -39,6 +69,12 @@ public class FrontendDeveloperTests {
      */
     public static boolean testTitleSearch() {
         try {
+            // SCENARIO 1: Test no match
+            ////////////////////////////
+
+
+            // SCENARIO 2: Test match
+            /////////////////////////
 
         } catch (Exception e) {
             System.out.println("FAILED: testTitleSearch(); unexpected exception");
@@ -54,6 +90,12 @@ public class FrontendDeveloperTests {
      */
     public static boolean testYearSearch() {
         try {
+            // SCENARIO 1: Test no match
+            ////////////////////////////
+
+
+            // SCENARIO 2: Test match
+            /////////////////////////
 
         } catch (Exception e) {
             System.out.println("FAILED: testYearSearch(); unexpected exception");
@@ -69,6 +111,30 @@ public class FrontendDeveloperTests {
      */
     public static boolean testFilterByProvider() {
         try {
+            // SCENARIO 1: Test valid input
+            ///////////////////////////////
+
+            // 1A: lowercase character
+
+            // 1B: uppercase character
+
+            // 1C: number
+
+            // 1D: quit
+
+
+            // SCENARIO 2: Test invalid input
+            /////////////////////////////////
+
+            // 2A: invalid lowercase character
+
+            // 2B: invalid uppercase character
+
+            // 2C: invalid number
+
+            // 2D: string longer than 1 character
+
+            // 2E: invalid character
 
         } catch (Exception e) {
             System.out.println("FAILED: testFilterByProvider(); unexpected exception");
